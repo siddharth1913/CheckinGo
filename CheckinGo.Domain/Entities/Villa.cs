573832457c94b74c23494fd4518e81db110f1676
Siddharth-1913 <98050209+siddharth1913@users.noreply.github.com>
2025-09-07 23:37:15 +0530

    Update and Delete operations for Villa

--- CheckinGo.Domain/Entities/Villa.cs
+++ CheckinGo.Domain/Entities/Villa.cs
@@ -10,13 +10,16 @@ namespace CheckinGo.Domain.Entites
     public class Villa
     {
         public int Id { get; set; }
+        [MaxLength(50)]
         public required string Name { get; set; }
         public string? Description { get; set; }
 
         // Display this name on UI forms
         [Display(Name = "Price Per Night")]
+        [Range(10,10000)]
         public double Price { get; set; }
         public int Sqft { get; set; }
+        [Range(1,10)]
         public int Occupancy { get; set; }
 
         [Display(Name = "Image Url")]
--- CheckinGo/Controllers/VillaController.cs
+++ CheckinGo/Controllers/VillaController.cs
@@ -23,15 +23,75 @@ namespace CheckinGo.Controllers
         }
 
         [HttpPost]
-        public IActionResult CreateVilla(Villa _villaObj )
+        public IActionResult CreateVilla(Villa _villaObj)
         {
+            if (_villaObj.Name == _villaObj.Description)
+            {
+                // Is key not specified then they it will show mssge on top of form because of ValidationSummary tag helper which is used in View
+                ModelState.AddModelError("Name", "The Name and Description cannot be the same.");
+            }
+
             if (ModelState.IsValid)
             {
                 _db.Villas.Add(_villaObj);
                 _db.SaveChanges();
+                TempData["success"] = "The villa has been created successfully";
                 return RedirectToAction("Index", "Villa");
             }
             return View(_villaObj);
+        }
+
+        // Get endpoint
+        public IActionResult Update(int? villaId)
+        {
+            Villa? villaObj = _db.Villas.FirstOrDefault(c => c.Id == villaId);
+            if (villaObj == null)
+            {
+                return RedirectToAction("Erro", "Home");
+            }
+            return View(villaObj);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Villa _villaObj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _db.Villas.Update(_villaObj);
+                _db.SaveChanges();
+                TempData["success"] = "The villa has been Updated successfully";
+                return RedirectToAction("Index", "Villa");
+            }
+            return View(_villaObj);
+        }
+
+        public IActionResult Delete(int? villaId)
+        {
+            Villa? villaObj = _db.Villas.FirstOrDefault(c => c.Id == villaId);
+            if (villaObj is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            return View(villaObj);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Villa _villaObj)
+        {
+            Villa? obj = _db.Villas.FirstOrDefault(c => c.Id == _villaObj.Id);
+            if (obj is not null)
+            {
+                _db.Villas.Remove(obj);
+                _db.SaveChanges();
+                TempData["success"] = "The villa has been deleted successfully";
+                return RedirectToAction("Index", "Villa");
+            }
+            TempData["error"] = "The villa could not be deleted";
+
+            return View();
+            //return View(obj);
         }
     }
 
